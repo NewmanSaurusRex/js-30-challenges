87b0f540b7703a6404154adb93f32dcd0727dc06
MNewman <megan.m.boggs@gmail.com>
2022-06-10 02:49:39 -0700

    updated colors of hands for accessibility, fix hour hand bug

--- css-js-clock/clock-app.js
+++ css-js-clock/clock-app.js
@@ -1,19 +1,18 @@
-let secondHand = document.querySelector(".second-hand");
-let minHand = document.querySelector(".min-hand");
-let hourHand = document.querySelector("hour-hand");
-
 function setDate() {
   let now = new Date();
   let seconds = now.getSeconds();
+  let secondHand = document.querySelector(".second-hand");
   let secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
   let mins = now.getMinutes();
   let minsDegrees = (mins / 60) * 360 + 90;
+  let minHand = document.querySelector(".min-hand");
   minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   let hours = now.getHours();
   let hoursDegrees = (hours / 12) * 360 + 90;
+  let hourHand = document.querySelector(".hour-hand");
   hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
